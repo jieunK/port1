e9084217abac77d59afc91ad6b3cf785769a965f
jieunK <90299417+jieunK@users.noreply.github.com>
2021-09-16 14:14:53 +0900

    210916

---
+++ pro1/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    //bxslider//
+    $('.slider').bxSlider({
+        pager:false,
+        controls:true,
+        autoStart:true,
+        pause:3000,//1000=1초//
+    });
+
+    
+    $('.cyber_imgset').bxSlider({
+        pager:false,
+        controls:false,
+        autoStart:true,
+        pause:3000,//1000=1초//
+    });
+
+    $('.rv1').bxSlider({
+        pager:true,
+        controls:true,
+        auto:true,
+        speed:3000,//1000=1초//
+        stopAutoOnClick:true,
+        autoControlsSelector:true,
+        pause:1000,
+        maxSlides:4,
+        slideWidth:300,
+    });
+}); //제이쿼리 끝//
---
+++ pro1/js/sub2.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    //bxslider//
+
+    $('.sc_1').bxSlider({
+        pager:false,
+        controls:true,
+        autoStart:true,
+        pause:3000,//1000=1초//
+        maxSlides:4,
+        slideWidth:330,
+    });
+}); //제이쿼리 끝//
